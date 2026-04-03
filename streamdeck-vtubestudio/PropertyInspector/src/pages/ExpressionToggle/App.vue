7e42e4866078ba69c2d92a0bf80c171c38e617e3
Cazzar <Cazzar@users.noreply.github.com>
2026-04-04 01:08:20 +1100

    Remove unreachable Reconnect button from PI pages

--- streamdeck-vtubestudio/PropertyInspector/src/pages/ExpressionToggle/App.vue
+++ streamdeck-vtubestudio/PropertyInspector/src/pages/ExpressionToggle/App.vue
@@ -18,8 +18,7 @@
   <div class="sdpi-item">
     <div class="sdpi-item-label">Tools</div>
     <button class="sdpi-item-value" id="force-refresh" @click="sendAction('refresh', null)">Refresh</button>
-    <button v-if="!websocketConnected" class="sdpi-item-value" @click="sendAction('force-reconnect', null)" id="force-reconnect">Reconnect</button>
-    <span v-else class="sdpi-item-value">VTS Connected!</span>
+    <span class="sdpi-item-value">VTS Connected!</span>
   </div>
 </div>
 </template>
