2e04d2ef0543e3898af592b66b2c4ab94c48e28c
Cazzar <Cazzar@users.noreply.github.com>
2026-08-29 00:15:23 +1000

    chore: drop unused function

--- VTubeStudio/Cazzar.Deck.VTubeStudio/Caches/ExpressionStatus.cs
+++ VTubeStudio/Cazzar.Deck.VTubeStudio/Caches/ExpressionStatus.cs
@@ -1,6 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Cazzar.Deck.VTubeStudio.Caches;
+﻿namespace Cazzar.Deck.VTubeStudio.Caches;
 
 public record ExpressionStatus
 {
@@ -9,12 +7,4 @@ public record ExpressionStatus
     public required string FileName { get; init; }
     public required bool IsActive { get; set; }
     public required string Name { get; init; }
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    private static string TrimSuffix(string suffix, string subject)
-    {
-        return subject.EndsWith(suffix, StringComparison.OrdinalIgnoreCase)
-            ? subject[..^suffix.Length]
-            : subject;
-    }
 }
